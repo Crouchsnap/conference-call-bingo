9d1413c2cedfbd1dd76a654cb54f517a2fdaaf1e
Andrew Crouch <jcrouc15@ford.com>
2020-03-19 14:56:51 -0400

    add all squares

--- src/Bingo.elm
+++ src/Bingo.elm
@@ -5,24 +5,43 @@ type alias Square =
     { value : String, checked : Bool }
 
 
-falseSquare text =
-    Square text False
-
-
 type alias Board =
     List Square
 
 
+falseSquare : String -> Square
+falseSquare text =
+    Square text False
+
+
+allSquares : List Square
 allSquares =
-    [ falseSquare "Mute your phone please"
-    , falseSquare "5 seconds awkward silence"
-    , falseSquare "People talking at the same time"
-    , falseSquare "Sound of telephone ringing"
-    , falseSquare "(sigh)"
-    , falseSquare "Ok, let's get started"
-    , falseSquare "(coughing)"
-    , falseSquare "Child or animal noise"
-    ]
+    List.map falseSquare
+        [ "\"Mute your phone please\""
+        , "\"Is ___ on the call?\""
+        , "5 seconds awkward silence"
+        , "People talking at the same time"
+        , "Sound of telephone ringing"
+        , "(sigh)"
+        , "\"Ok, let's get started\""
+        , "(coughing)"
+        , "Child or animal noise"
+        , "\"This was shared ahead of time\""
+        , "Echo noise or feedback"
+        , "Sound of someone typing"
+        , "\"Can you repeat that?\""
+        , "\"Can you share that by email?\""
+        , "Sound of background conversation"
+        , "\"...technical difficulties\""
+        , "5th \"Thank you\""
+        , "\"Time is just about up\""
+        , "Powerpoint malfunction"
+        , "\"I was on mute\""
+        , "\"Can you see my screen?\""
+        , "Unintended interruption"
+        , "\"___ can you comment?\""
+        , "\"I need to step out...\""
+        ]
 
 
 board : Board
