e0822ac80686c2a1ca638048404e89bf06f8fff8
Chris Dorman <cdorman1@ford.com>
2020-03-23 11:10:55 -0400

    Adding annotation

--- backend/src/main/kotlin/com/bingo/ConferenceCallBingoApplication.kt
+++ backend/src/main/kotlin/com/bingo/ConferenceCallBingoApplication.kt
@@ -2,8 +2,10 @@ package com.bingo
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.data.mongodb.repository.config.EnableMongoRepositories
 
 @SpringBootApplication
+@EnableMongoRepositories
 class ConferenceCallBingoApplication
 
 fun main(args: Array<String>) {
