a0af65c22aed495ee5c25113645043a65903e943
Chris Dorman <cdorman1@ford.com>
2020-03-23 12:47:47 -0400

    adding cors stuff

--- backend/src/main/kotlin/com/bingo/high/score/HighScoreController.kt
+++ backend/src/main/kotlin/com/bingo/high/score/HighScoreController.kt
@@ -5,6 +5,7 @@ import org.springframework.web.bind.annotation.*
 
 @RestController
 @RequestMapping(path = ["/highscores"])
+@CrossOrigin(origins = ["http://localhost:8000", "https://bingo.apps.pd01.useast.cf.ford.com"])
 class HighScoreController {
     
     @Autowired
@@ -19,5 +20,4 @@ class HighScoreController {
         highScoreRepository.save(score.toHighScoreEntity())
     }
 }
-
 
