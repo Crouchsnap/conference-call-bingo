0bd3045d16f785d2bbe94e007a98d38a18ad8709
Chris Dorman <cdorman1@ford.com>
2020-04-21 18:06:45 -0400

    Refactored theme class

--- ui/src/Options/Theme.elm
+++ ui/src/Options/Theme.elm
@@ -1,4 +1,4 @@
-module Options.Theme exposing (SelectedTheme, Theme(..), normalizedTheme, systemTheme, themedClass)
+module Options.Theme exposing (Theme(..), normalizedTheme, systemTheme, themedClass)
 
 import Html exposing (Attribute)
 import Html.Attributes exposing (class)
@@ -8,12 +8,6 @@ type Theme
     = Light
     | Dark
     | System Theme
-
-
-type alias SelectedTheme =
-    { system : Theme
-    , selected : Theme
-    }
 
 
 systemTheme : Bool -> Theme
@@ -38,17 +32,13 @@ normalizedTheme selected =
 
 themedClass : Theme -> String -> Attribute msg
 themedClass applyTheme className =
-    let
-        classes =
-            className |> String.split " "
-    in
     case applyTheme |> normalizedTheme of
         Dark ->
-            class
-                (classes
-                    |> List.map (\name -> name ++ " " ++ name ++ "-dark")
-                    |> String.join " "
-                )
+            className
+                |> String.split " "
+                |> List.map (\name -> name ++ " " ++ name ++ "-dark")
+                |> String.join " "
+                |> class
 
         _ ->
-            class (classes |> String.join " ")
+            class className
