d3d6bcf2a30b093fe4703b3171eb078893a6edc8
Chris Dorman <cdorman1@ford.com>
2020-04-22 21:18:31 -0400

    Moved square text to topics

--- ui/src/Game/Square.elm
+++ ui/src/Game/Square.elm
@@ -1,7 +1,7 @@
 module Game.Square exposing (Square, centerSquare, checked, genericSquare, squaresByTopic, toggleSquareInList)
 
 import Game.Dot as Dot exposing (Dot, dot)
-import Game.Topic exposing (Topic(..))
+import Game.Topic as Topic exposing (Topic(..))
 import Html exposing (Html, br, div)
 import Random exposing (Seed)
 
@@ -20,8 +20,8 @@ fordismSquare text =
     Square (Html.text text) Fordism []
 
 
-coronavirus : String -> Square msg
-coronavirus text =
+coronavirusSquare : String -> Square msg
+coronavirusSquare text =
     Square (Html.text text) Coronavirus []
 
 
@@ -32,89 +32,26 @@ centerSquare =
 
 genericSquares : List (Square msg)
 genericSquares =
-    [ genericSquare "\"Mute your phone please\""
-    , genericSquare "5 seconds awkward silence"
-    , genericSquare "Sound of telephone ringing"
-    , genericSquare "(sigh)"
-    , genericSquare "\"Ok, let's get started\""
-    , genericSquare "(coughing)"
-    , genericSquare "Child or animal noise"
-    , genericSquare "People talking at the same time"
-    , genericSquare "\"This was shared ahead of time\""
-    , genericSquare "Echo noise or feedback"
-    , genericSquare "Sound of someone typing"
-    , genericSquare "\"Can you repeat that?\""
-    , genericSquare "\"Can you share that by email?\""
-    , genericSquare "Background talking"
-    , genericSquare "\"...technical difficulties\""
-    , genericSquare "5th \"Thank you\""
-    , genericSquare "\"Time is just about up\""
-    , genericSquare "Powerpoint malfunction"
-    , genericSquare "\"I was on mute\""
-    , genericSquare "\"Can you see my screen?\""
-    , genericSquare "Unintended interruption"
-    , genericSquare "\"__, can you comment?\""
-    , genericSquare "\"I need to step out\""
-    , genericSquare "\"Can you share?\""
-    , genericSquare "\"You're not sharing\""
-    , genericSquare "Any acronym"
-    , genericSquare "Roundtable intros"
-    , genericSquare "5 or more Webex beeps"
-    , genericSquare "Pet or child on webcam"
-    , genericSquare "University logo on webcam"
-    , genericSquare "\"What team are you from?\""
-    , genericSquare "\"double mute\""
-    , genericSquare "Reference to people as resources"
-    , genericSquare "\"I'll send out the deck\""
-    , genericSquare "(eating sounds)"
-    , genericSquare "\"Bingo!\""
-    , genericSquare "\"I think ____ stepped away\""
-    , genericSquare "\"Can you hear me?\""
-    , genericSquare "(breathing directly into the microphone)"
-    , genericSquare "\"That's a great question...(deflection)\""
-    , genericSquare "Two people with the same first name present"
-    ]
+    Topic.generic |> List.map genericSquare
 
 
-fordisms : List (Square msg)
-fordisms =
-    [ fordismSquare "\"VIN number\""
-    , fordismSquare "\"MVP\""
-    , fordismSquare "Any vehicle program number"
-    , fordismSquare "\"Go further\""
-    , fordismSquare "\"LL_ approval needed\""
-    , fordismSquare "Any _aaS acronym"
-    , fordismSquare "A well known acronym repurposed by Ford"
-    , fordismSquare "Now, near, far"
-    , fordismSquare "\"I'll send a note\""
-    , fordismSquare "First name reference to an exec you don't know"
-    , fordismSquare "Five or more people named for Disciples present"
-    , fordismSquare "The only mens' shirts visible are white or blue"
-    ]
+fordismSquares : List (Square msg)
+fordismSquares =
+    Topic.fordisms |> List.map fordismSquare
 
 
-coronaviri : List (Square msg)
-coronaviri =
-    [ coronavirus "TP"
-    , coronavirus "Corona"
-    , coronavirus "Virus"
-    , coronavirus "Hand Sanitizer"
-    , coronavirus "Wash your hands"
-    , coronavirus "Sick"
-    , coronavirus "\"does ___ store have supplies?\""
-    , coronavirus "Death rate"
-    , coronavirus "Recovery rate"
-    , coronavirus "Work from home"
-    ]
+coronavirusSquares : List (Square msg)
+coronavirusSquares =
+    Topic.coronaviri |> List.map coronavirusSquare
 
 
-allTopics =
-    fordisms ++ coronaviri
+allTopicSquares =
+    fordismSquares ++ coronavirusSquares
 
 
 squaresByTopic : List Topic -> List (Square msg)
 squaresByTopic topics =
-    genericSquares ++ (allTopics |> List.filter (\square -> List.member square.topic topics))
+    genericSquares ++ (allTopicSquares |> List.filter (\square -> List.member square.topic topics))
 
 
 toggleSquareInList : Seed -> Dot.Color -> Square msg -> List (Square msg) -> ( List (Square msg), Seed )
--- ui/src/Game/Topic.elm
+++ ui/src/Game/Topic.elm
@@ -1,4 +1,4 @@
-module Game.Topic exposing (Topic(..), encodeTopic, toString, toggleTopic, topicDecoder)
+module Game.Topic exposing (Topic(..), coronaviri, encodeTopic, fordisms, generic, toString, toggleTopic, topicDecoder)
 
 import Json.Decode as Decode exposing (Decoder)
 import Json.Encode as Encode
@@ -10,6 +10,84 @@ type Topic
     | Fordism
     | Coronavirus
     | Center
+
+
+generic : List String
+generic =
+    [ "\"Mute your phone please\""
+    , "5 seconds awkward silence"
+    , "Sound of telephone ringing"
+    , "(sigh)"
+    , "\"Ok, let's get started\""
+    , "(coughing)"
+    , "Child or animal noise"
+    , "People talking at the same time"
+    , "\"This was shared ahead of time\""
+    , "Echo noise or feedback"
+    , "Sound of someone typing"
+    , "\"Can you repeat that?\""
+    , "\"Can you share that by email?\""
+    , "Background talking"
+    , "\"...technical difficulties\""
+    , "5th \"Thank you\""
+    , "\"Time is just about up\""
+    , "Powerpoint malfunction"
+    , "\"I was on mute\""
+    , "\"Can you see my screen?\""
+    , "Unintended interruption"
+    , "\"__, can you comment?\""
+    , "\"I need to step out\""
+    , "\"Can you share?\""
+    , "\"You're not sharing\""
+    , "Any acronym"
+    , "Roundtable intros"
+    , "5 or more Webex beeps"
+    , "Pet or child on webcam"
+    , "University logo on webcam"
+    , "\"What team are you from?\""
+    , "\"double mute\""
+    , "Reference to people as resources"
+    , "\"I'll send out the deck\""
+    , "(eating sounds)"
+    , "\"Bingo!\""
+    , "\"I think ____ stepped away\""
+    , "\"Can you hear me?\""
+    , "(breathing directly into the microphone)"
+    , "\"That's a great question...(deflection)\""
+    , "Two people with the same first name present"
+    ]
+
+
+fordisms : List String
+fordisms =
+    [ "\"VIN number\""
+    , "\"MVP\""
+    , "Any vehicle program number"
+    , "\"Go further\""
+    , "\"LL_ approval needed\""
+    , "Any _aaS acronym"
+    , "A well known acronym repurposed by Ford"
+    , "Now, near, far"
+    , "\"I'll send a note\""
+    , "First name reference to an exec you don't know"
+    , "Five or more people named for Disciples present"
+    , "The only mens' shirts visible are white or blue"
+    ]
+
+
+coronaviri : List String
+coronaviri =
+    [ "TP"
+    , "Corona"
+    , "Virus"
+    , "Hand Sanitizer"
+    , "Wash your hands"
+    , "Sick"
+    , "\"does ___ store have supplies?\""
+    , "Death rate"
+    , "Recovery rate"
+    , "Work from home"
+    ]
 
 
 toggleTopic topic topics =
