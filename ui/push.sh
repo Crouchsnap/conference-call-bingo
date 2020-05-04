0408e4dd12b084505157bd0ed7e340dd2ce2d573
Chris Dorman <cdorman1@ford.com>
2020-05-04 15:05:01 -0400

    Optimized and ugified elm stuff

--- ui/push.sh
+++ ui/push.sh
@@ -1,4 +1,22 @@
 #!/bin/bash
 
-elm make src/Main.elm --output=main.js
+set -e
+
+which uglifyjs
+
+if ($? > 0) then
+  npm install uglify-js --global
+fi
+
+js="main.js"
+
+elm make --optimize --output=$js src/Main.elm
+echo "Initial size: $(cat $js | wc -c) bytes  ($js)"
+
+uglifyjs $js --compress "pure_funcs=[F2,F3,F4,F5,F6,F7,F8,F9,A2,A3,A4,A5,A6,A7,A8,A9],pure_getters,keep_fargs=false,unsafe_comps,unsafe" | uglifyjs --mangle --output=$js
+
+echo "Minified size:$(cat $js | wc -c) bytes  ($js)"
+echo "Gzipped size: $(cat $js | gzip -c | wc -c) bytes"
+
+
 cf push bingo -b staticfile_buildpack --no-manifest
